7e1b12ebab60d38578c7b9e4740aab4f22e207c8
MohitSingh65 <mohitsinghpanwar65@gmail.com>
2025-02-28 16:07:05 +0530

    Add feature to set reminder time in 24-hour format

--- remindme.go
+++ remindme.go
@@ -7,6 +7,14 @@ import (
 	"time"
 )
 
+func sendNotification(message string) {
+	cmd := exec.Command("notify-send", "remindme", message)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error sending notification.", err)
+	}
+}
+
 func setReminder(message string, duration time.Duration) {
 	cmd := exec.Command("sh", "-c", fmt.Sprintf(
 		"nohup sleep %d && notify-send remindme '%s' &",
@@ -21,27 +29,48 @@ func setReminder(message string, duration time.Duration) {
 	}
 }
 
+func parseTime(input string) (time.Duration, error) {
+	layout := "15:04"
+	targetTime, err := time.Parse(layout, input)
+	if err != nil {
+		return 0, fmt.Errorf("invalid time format. HH:MM (24-hour format)")
+	}
+	now := time.Now()
+
+	targetTime = time.Date(now.Year(), now.Month(), now.Day(), targetTime.Hour(), targetTime.Minute(), 0, 0, now.Location())
+
+	if targetTime.Before(now) {
+		targetTime = targetTime.Add(24 * time.Hour)
+	}
+
+	duration := targetTime.Sub(now)
+	return duration, nil
+}
+
 func main() {
 	message := flag.String("message", "", "The reminder message")
 	minutes := flag.Int("minutes", 0, "Time in minutes for the reminder")
 	hours := flag.Int("hours", 0, "Time in hours for the reminder")
+	exactTime := flag.String("at", "", "Exact time for the reminder (HH:MM)")
 	flag.Parse()
 
-	if *message == "" {
-		fmt.Println("Reminder message is required.")
-		flag.Usage()
-		return
-	}
+	var duration time.Duration
+	var err error
 
-	if *minutes == 0 && *hours == 0 {
+	if *exactTime != "" {
+		duration, err = parseTime(*exactTime)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	} else if *minutes > 0 || *hours > 0 {
+		duration = time.Duration(*hours)*time.Hour + time.Duration(*minutes)*time.Minute
+	} else {
 		fmt.Println("Error: You must specify a time for the reminder.")
 		flag.Usage()
 		return
 	}
-
-	duration := time.Duration(*hours)*time.Hour + time.Duration(*minutes)*time.Minute
-
 	fmt.Printf("Reminder set for %v from now.\n", duration)
-
 	setReminder(*message, duration)
 }
